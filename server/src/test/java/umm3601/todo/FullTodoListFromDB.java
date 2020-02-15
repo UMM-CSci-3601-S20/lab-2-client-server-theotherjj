3d9e2fe4473b4811976a8940cedf68b61060f377
JoshuaHamann <haman127@morris.umn.edu>
2020-02-15 00:14:49 -0600

    Modify some testing for fulltodolist; none pass

---
+++ server/src/test/java/umm3601/todo/FullTodoListFromDB.java
@@ -0,0 +1,23 @@
+package umm3601.todo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests umm3601.user.Database listUser functionality
+ */
+public class FullTodoListFromDB {
+
+  @Test
+  public void firstTodoInFullList() throws IOException {
+    Database db = new Database("/todos.json");
+    Todo[] allTodos = db.listTodos(new HashMap<>());
+    Todo firstTodo = allTodos[0];
+    assertEquals("Blanche", firstTodo.owner, "Incorrect name");
+    assertEquals("software design", firstTodo.category, "Incorrect e-mail");
+  }
+}
