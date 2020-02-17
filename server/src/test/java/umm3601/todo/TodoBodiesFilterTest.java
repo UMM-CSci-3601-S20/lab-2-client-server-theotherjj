ff433092de1ef1cf7421d1a05a742eeb8ef457cc
Johannes Martinez <mart4695@morris.umn.edu>
2020-02-17 13:53:29 -0600

    Uncomment multiple (working) tests in TodoBodiesFilterTest.java
    Note: We talked to professor McPhee about uncommenting these tests after the deadline

--- server/src/test/java/umm3601/todo/TodoBodiesFilterTest.java
+++ server/src/test/java/umm3601/todo/TodoBodiesFilterTest.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.io.IOException;
-//import java.util.Arrays;
+import java.util.Arrays;
 import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
+import java.util.List;
+import java.util.Map;
 
 //import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -31,37 +31,37 @@ public class TodoBodiesFilterTest{
     assertEquals("software design", firstTempor.category, "Incorrect category for firstTempor");
   }
 
-  // @Test
-  // public void bodyFilterWithStatus() throws IOException{
-  //   TodoDatabase db = new TodoDatabase("/todos.json");
-  //   Map<String, List<String>> queryParams = new HashMap<>();
+  @Test
+  public void bodyFilterWithStatus() throws IOException{
+    TodoDatabase db = new TodoDatabase("/todos.json");
+    Map<String, List<String>> queryParams = new HashMap<>();
 
-  //   queryParams.put("contains", Arrays.asList(new String[] {"tempor ullamco"}));
-  //   queryParams.put("status", Arrays.asList(new String[] { "complete" }));
-  //   Todo[] completeTodosTempor = db.listTodos(queryParams);
-  //   System.out.println(completeTodosTempor.length);
-  //   Todo thirdCompleteTemporTodo = completeTodosTempor[2];
-  //   System.out.println(thirdCompleteTemporTodo.owner);
-  //   System.out.println(thirdCompleteTemporTodo.category);
-  //   System.out.println(thirdCompleteTemporTodo._id);
-  //   System.out.println(thirdCompleteTemporTodo.status);
-  //   assertEquals(3, completeTodosTempor.length, "Incorrect number of todos with complete status");
-  //   assertEquals("58895985839dd0a53b16bf3f", thirdCompleteTemporTodo._id, "Incorrect id with tempor and complete");
-  //   assertEquals("Roberta", thirdCompleteTemporTodo.owner, "Incorrect owner with tempor and complete");
-  // }
+    queryParams.put("contains", Arrays.asList(new String[] {"tempor ullamco"}));
+    queryParams.put("status", Arrays.asList(new String[] { "complete" }));
+    Todo[] completeTodosTempor = db.listTodos(queryParams);
+    System.out.println(completeTodosTempor.length);
+    Todo thirdCompleteTemporTodo = completeTodosTempor[2];
+    System.out.println(thirdCompleteTemporTodo.owner);
+    System.out.println(thirdCompleteTemporTodo.category);
+    System.out.println(thirdCompleteTemporTodo._id);
+    System.out.println(thirdCompleteTemporTodo.status);
+    assertEquals(3, completeTodosTempor.length, "Incorrect number of todos with complete status");
+    assertEquals("58895985839dd0a53b16bf3f", thirdCompleteTemporTodo._id, "Incorrect id with tempor and complete");
+    assertEquals("Roberta", thirdCompleteTemporTodo.owner, "Incorrect owner with tempor and complete");
+  }
 
-  // @Test
-  // public void bodyFilterWithLimit() throws IOException{
-  //   TodoDatabase db = new TodoDatabase("/todos.json");
-  //   Map<String, List<String>> queryParams = new HashMap<>();
-  //   queryParams.put("contains", Arrays.asList(new String[] {"tempor ullamco"}));
-  //   queryParams.put("limit", Arrays.asList(new String[] { "2" }));
-  //   Todo[] limitTodosTempor = db.listTodos(queryParams);
-  //   System.out.println(limitTodosTempor.length);
-  //   Todo secondLimitTemporTodo = limitTodosTempor[1];
-  //   assertEquals(2, limitTodosTempor.length, "Incorrect number of limit Tempor todos");
-  //   assertEquals("58895985daa7d137399e8c58", secondLimitTemporTodo._id, "Incorrect id of limit Tempor todos");
-  //   assertEquals(true, secondLimitTemporTodo.status, "Incorrect status of limit Tempor todos");
-  // }
+  @Test
+  public void bodyFilterWithLimit() throws IOException{
+    TodoDatabase db = new TodoDatabase("/todos.json");
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("contains", Arrays.asList(new String[] {"tempor ullamco"}));
+    queryParams.put("limit", Arrays.asList(new String[] { "2" }));
+    Todo[] limitTodosTempor = db.listTodos(queryParams);
+    System.out.println(limitTodosTempor.length);
+    Todo secondLimitTemporTodo = limitTodosTempor[1];
+    assertEquals(2, limitTodosTempor.length, "Incorrect number of limit Tempor todos");
+    assertEquals("58895985daa7d137399e8c58", secondLimitTemporTodo._id, "Incorrect id of limit Tempor todos");
+    assertEquals(true, secondLimitTemporTodo.status, "Incorrect status of limit Tempor todos");
+  }
 
 }
